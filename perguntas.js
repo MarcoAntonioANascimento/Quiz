7130d6ac6a54da0e94a7a891d12484805ba9bf1f
MarcoAntonioANascimento <00001122664606sp@al.educacao.sp.gov.br>
2024-11-28 15:26:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Matemática',
+    'Qual a raiz de 36?',
+    '6'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz arroz em Inglês?',
+    'Arroz em ingles é Rice'
+)
+criaCartao(
+    'Lingua portuguesa',
+    'Qual o sinônimo de preguiçoso?',
+    'Folgado, Encostado, Desocupado,etc'
+)
